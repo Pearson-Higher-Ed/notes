9ca9a65925747ed1f124206eedeee3ba085221ec
Arish Kumar K <Arish@Arishs-MacBook-Pro.local>
2016-12-02 12:48:05 +0530

    added svg and removed png

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,11 @@ module.exports = {
       {
         test: /\.(woff|png|jpg|gif)$/,
         loader: 'url-loader?limit=10000'
-      }
+      },
+      {
+       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+       loader: 'url?mimetype=image/svg+xml'
+     }
     ]
   },
   resolve: {
