98b086ddd66ca3eff92df7a2de960687f5e7e3fa
Kamatham, Chaitanya Kumar <Chaitanyakumar.Kamatham@pearson.com>
2017-05-19 02:15:12 -0700

    Fixed Lint errors, Demo
    * Fixed lint errors, demo
    * Updated eslint configuration (similar to vega)
    * Updated node-sass, sass-loaders due to fatal error caused when running the project
    * Added prop-types dependency as react suggested it
    * Updated peer dependencies as warned by lint errors

--- demo/demo.js
+++ demo/demo.js
@@ -4,6 +4,8 @@ import enLocaleData from 'react-intl/locale-data/en';
 import frLocaleData from 'react-intl/locale-data/fr';
 import tsLocaleData from 'react-intl/locale-data/ts';
 
+/* eslint-disable no-alert */
+
 const localeData = {
   en: enLocaleData,
   fr: frLocaleData,
@@ -11,37 +13,97 @@ const localeData = {
 };
 
 function getParam(item) {
-  const svalue = location.search.match(new RegExp('[\?\&]' + item + '=([^\&]*)(\&?)', 'i'));
+  const svalue = window.location.search.match(new RegExp(`[?&]${item}=([^&]*)(&?)`, 'i'));
   return svalue ? svalue[1] : svalue;
 }
 
-window.onload = init;
 
-window.noteClickCbk = function() {
-  alert('Clicked on Note');
-};
+function init() {
+  injectTapEventPlugin();
+  // Mock data for notes component
+  const mockData = [
+    {
+      id: 1,
+      pageId: 1,
+      author: 'Pete Hunt',
+      time: 86400000,
+      text: `This is the first note and its about two lines long ifdsfd sfdfdfdfd fda fd fdsf dsaf dsaf ds fdsa
+       fsffe wafeaw feaw fewa fewa fea fewafea feaw feaw feaw dsa fdsfds fdsaf dsaf ewaf ewaf ewa fewa fewa fewa 
+       ewa few fewaf ewa fewa fewa fewa few few afewa fe fewa feaw fea faewa fefwa fes the first highlighted text`,
+      comment: '',
+      color: 'Green'
+    },
+    {
+      id: 2,
+      pageId: 1,
+      author: 'Bob Schumer',
+      time: 86400000,
+      text: `This is the second note feaw fea feaw fea fea fea fea feafef w afewaf e ewafeaw end highl fewa fewaf ewa 
+      fewaf ewaf ewa fewa few fewa fewa fewa fewa fewafe wa feawf edfdsfdsafewe fewa fewa fea fea feaw fea fewa fea 
+      afewafew afeaw fewa fewa fewa fewa fewafa fea fea fewa fewa feaw feaw fewa fewa fewa fewa feafeafes ighted text`,
+      comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi ligula, dapibus a volutpat sit amet,
+       mattis et dui. Nunc porttitor accumsan orci id luctus. Phasellus ipsum metus, tincidunt non rhoncus id, dictum a 
+       lectus. Nam sed ipsum a lacus sodales eleifend. Vestibulum lorem felis, rhoncus elementum vestibulum eget, 
+       dictum ut velit. Nullam venenatis, elit in suscipit imperdiet, orci purus posuere mauris, quis adipiscing ipsum 
+       urna ac quam.`,
+      color: 'Green'
+    },
+    {
+      id: 3,
+      pageId: 1,
+      author: 'Jay Williams',
+      time: 86400000,
+      text: 'This is the third notes highlighted text',
+      comment: `It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of 
+      foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the 
+      season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had 
+      nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the 
+      period was so far like the present period, that some of its noisiest authorities insisted on its being received, 
+      for good or for evil, in the superlative degree of comparison only.`,
+      color: 'Pink'
+    },
+    {
+      id: 4,
+      pageId: 1,
+      author: 'Jordan Walke',
+      time: 86400000,
+      text: 'This is the fourth notes highlighted text',
+      comment: '',
+      color: 'Yellow'
+    },
+    {
+      id: 5,
+      pageId: 1,
+      author: 'Shandy Miller',
+      time: 86400000,
+      text: 'This is the fifth notes highlighted text',
+      comment: 'This is the fifth from the instructor',
+      color: 'Instructor'
+    }
+  ];
 
-function init() {  
-  injectTapEventPlugin(); 
-  // Mock data for notes component  
-  const mockData = [{id: 1, author: 'Pete Hunt', time: 86400000, text: 'This is the first note and its about two lines long ifdsfd sfdfdfdfd fda fd fdsf dsaf dsaf ds fdsa fsffe wafeaw feaw fewa fewa fea fewafea feaw feaw feaw dsa fdsfds fdsaf dsaf ewaf ewaf ewa fewa fewa fewa ewa few fewaf ewa fewa fewa fewa few few afewa fe fewa feaw fea faewa fefwa fes the first highlighted text', comment: '', color: 'Green'},
-                    {id: 2, author: 'Bob Schumer', time: 86400000, text: 'This is the second note feaw fea feaw fea fea fea fea feafef w afewaf e ewafeaw end highl fewa fewaf ewa fewaf ewaf ewa fewa few fewa fewa fewa fewa fewafe wa feawf edfdsfdsafewe fewa fewa fea fea feaw fea fewa fea afewafew afeaw fewa fewa fewa fewa fewafa fea fea fewa fewa feaw feaw fewa fewa fewa fewa feafeafes ighted text', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi ligula, dapibus a volutpat sit amet, mattis et dui. Nunc porttitor accumsan orci id luctus. Phasellus ipsum metus, tincidunt non rhoncus id, dictum a lectus. Nam sed ipsum a lacus sodales eleifend. Vestibulum lorem felis, rhoncus elementum vestibulum eget, dictum ut velit. Nullam venenatis, elit in suscipit imperdiet, orci purus posuere mauris, quis adipiscing ipsum urna ac quam.', color: 'Green'},
-                    {id: 3, author: 'Jay Williams', time: 86400000, text: 'This is the third notes highlighted text', comment: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.', color: 'Pink'},
-                    {id: 4, author: 'Jordan Walke', time: 86400000, text: 'This is the fourth notes highlighted text', comment: '', color: 'Yellow'},
-                    {id: 5, author: 'Shandy Miller', time: 86400000, text: 'This is the fifth notes highlighted text', comment: 'This is the fifth from the instructor', color: 'Instructor'}];   
- 
   const region = getParam('lang') || 'en';
   addLocaleData(localeData[region.split('-')[0]]);
-  
+
   // Create new instance of notes component
-  document.body.dispatchEvent(new CustomEvent('o.InitNotes', {
+  document.body.dispatchEvent(new window.CustomEvent('o.InitNotes', {
     detail: {
-      elementId: 'notes-demo',   
+      elementId: 'notes-demo',
       locale: region,
       notes: mockData,
-      clickNoteHandler : window.noteClickCbk
+      clickNoteHandler: window.noteClickCbk,
+      removeNoteHandler: () => {},
+      drawerCallbacks: {
+        onActive: () => {},
+        changeState: () => {}
+      }
     }
   }));
 }
 
+window.onload = init;
+
+window.noteClickCbk = () => {
+  window.alert('Clicked on Note');
+};
 
--- src/js/Note.jsx
+++ src/js/Note.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import SvgIcon from 'material-ui/SvgIcon';
@@ -19,27 +19,46 @@ export default class Note extends React.Component {
     };
   }
 
-  handleModalOpen = () =>  {
-    this.setState({modalOpen: true});
+  handleModalOpen = (e) => {
+    this.setState({ modalOpen: true });
+    if (e && e.preventDefault) {
+      e.preventDefault();
+    }
   };
 
-  handleModalClose = () =>  {
-    this.setState({modalOpen: false});
+  handleModalClose = (e) => {
+    this.setState({ modalOpen: false });
+    if (e && e.preventDefault) {
+      e.preventDefault();
+    }
   };
 
   handleNoteClick(pageId, e) {
     this.props.noteClick(pageId);
     const listDom = Array.from(document.getElementsByClassName('note-row'));
-    listDom.map((node) => node.className = 'note-row');
+    listDom.map(node => node.setAttribute('class', 'note-row'));
     e.target.parentNode.classList.add('focused');
     AnalyticsManager.dispatch({
       category: 'Notes',
       action: 'Click',
       label: JSON.stringify({ PageId: pageId })
-    })
+    });
+    if (e && e.preventDefault) {
+      e.preventDefault();
+    }
   }
-  arrowKeyPress(e) {
-    if (e.which === 37 || e.keyCode === 37 || e.which === 38 || e.keyCode === 38 || e.which === 39 || e.keyCode === 39 || e.which === 40 || e.keyCode === 40) { // down Arrow key
+  static arrowKeyPress(e) {
+    if (
+      e.which === 37 ||
+      e.keyCode === 37 ||
+      e.which === 38 ||
+      e.keyCode === 38 ||
+      e.which === 39 ||
+      e.keyCode === 39 ||
+      e.which === 40 ||
+      e.keyCode === 40
+    ) {
+      // down Arrow key
       e.preventDefault();
       e.target.parentNode.classList.remove('focused');
     }
@@ -52,36 +71,35 @@ export default class Note extends React.Component {
       category: 'Notes',
       action: 'Remove',
       label: JSON.stringify({ NoteId: noteId })
-    })
+    });
   }
 
   renderIcon(commentExists) {
     if (commentExists) {
-      return(
-        <i className={this.props.color.toLowerCase() + '-icon color-icon'}>
-                <span className="reader-only">{this.props.color} Highlight</span>
-        </i>
-      )
-    }
-    else {
       return (
-        <span className={this.props.color + ' color-dot'}>
-            <span className="reader-only">
-                {this.props.color} Highlight
-            </span>
-        </span>
-      )
+        <i className={`${this.props.color.toLowerCase()}-icon color-icon`}>
+          <span className="reader-only">{this.props.color} Highlight</span>
+        </i>
+      );
     }
+
+    return (
+      <span className={`${this.props.color} color-dot`}>
+        <span className="reader-only">
+          {this.props.color} Highlight
+        </span>
+      </span>
+    );
   }
-  onFocus(e) {
+  static onFocus(e) {
     e.target.parentNode.classList.add('focused');
     return true;
   }
   onBlur(e) {
     e.target.parentNode.className = 'note-row';
-    const current_note = document.getElementsByClassName('note-row')
-    const last_element = current_note[current_note.length - 1];
-    if (e.target.parentNode === last_element) {
+    const currentNote = document.getElementsByClassName('note-row');
+    const lastElement = currentNote[currentNote.length - 1];
+    if (e.target.parentNode === lastElement) {
       this.props.drawerCallbacks.onActive('contents');
       this.props.drawerCallbacks.changeState(0);
     }
@@ -93,7 +111,7 @@ export default class Note extends React.Component {
       document.getElementsByClassName('handleCloseIcon')[0].focus();
       event.preventDefault();
     }
-  }
+  };
 
   cancelIconKeySelect = (event) => {
     if ((event.which || event.keyCode) === 13) {
@@ -103,15 +121,15 @@ export default class Note extends React.Component {
       document.getElementsByClassName('deleteBtn')[0].focus();
       event.preventDefault();
     }
-  }
+  };
 
   render() {
     const that = this;
     const { formatMessage, formatDate, formatTime } = that.props.intl;
-    const commentExists = (this.props.comment) ? true : false;
+    const commentExists = !!(this.props.comment);
 
     const DialogStyle = {
-      dialogContainerstyl : {
+      dialogContainerstyl: {
         width: '362px',
         backgroundColor: '#fff',
         borderRadius: '4px'
@@ -125,94 +143,126 @@ export default class Note extends React.Component {
         width: '18px',
         cursor: 'pointer'
       },
-      cancelbtnstyl : {
+      cancelbtnstyl: {
         color: '#74797b'
       },
-      deleteBtnstyl : {
+      deleteBtnstyl: {
         borderRadius: 2,
         backgroundColor: '#34b6b4'
       }
-    }
+    };
     const actions = [
       <FlatButton
         label={formatMessage(messages.cancel)}
-        primary={true}
+        primary
         style={DialogStyle.cancelbtnstyl}
         className="cancelBtn"
-        onClick={this.handleModalClose} />,
+        onClick={this.handleModalClose}
+      />,
       <FlatButton
         label={formatMessage(messages.delete)}
-        primary={true}
-        autoFocus={true}
-        onClick={this.handleDeleteClick.bind(this, this.props.id)}
+        primary
+        autoFocus
+        onClick={e => this.handleDeleteClick(this.props.id, e)}
         onKeyDown={this.dialogKeySelect}
         style={DialogStyle.deleteBtnstyl}
-        className="deleteBtn" />
+        className="deleteBtn"
+      />
     ];
-    const CancelIcon = (props) => (
+    const CancelIcon = props => (
       <SvgIcon {...props}>
-        <path d="M712.993036,23.3253012 L720.736289,15.2808193 C721.026024,14.9878313 721.026024,14.5128916 720.735855,14.2199036 C720.445687,13.9266988 719.975518,13.9266988 719.685349,14.2199036 L711.976795,22.005012 L704.268458,14.2199036 C703.978072,13.9266988 703.50812,13.9266988 703.217735,14.2199036 C702.927566,14.5128916 702.927566,14.9878313 703.217518,15.2808193 L710.960554,23.3253012 L703.217735,31.3697831 C702.927566,31.6627711 702.927566,32.1377108 703.217735,32.4306988 C703.507687,32.7234699 703.978072,32.7239036 704.268458,32.4306988 L711.976795,24.6455904 L719.685349,32.4306988 C719.975518,32.7239036 720.44612,32.7234699 720.735855,32.4306988 C721.026024,32.1377108 721.026024,31.6627711 720.735855,31.3697831 L712.993036,23.3253012 Z" id="ic_cancel" fill="#8d8d8d"></path>
+        <path
+          d="M712.993036,23.3253012 L720.736289,15.2808193 C721.026024,14.9878313 721.026024,14.5128916 720.735855,
+          14.2199036 C720.445687,13.9266988 719.975518,13.9266988 719.685349,14.2199036 L711.976795,
+          22.005012 L704.268458,14.2199036 C703.978072,13.9266988 703.50812,13.9266988 703.217735,
+          14.2199036 C702.927566,14.5128916 702.927566,14.9878313 703.217518,15.2808193 L710.960554,
+          23.3253012 L703.217735,31.3697831 C702.927566,31.6627711 702.927566,32.1377108 703.217735,
+          32.4306988 C703.507687,32.7234699 703.978072,32.7239036 704.268458,32.4306988 L711.976795,
+          24.6455904 L719.685349,32.4306988 C719.975518,32.7239036 720.44612,32.7234699 720.735855,
+          32.4306988 C721.026024,32.1377108 721.026024,31.6627711 720.735855,31.3697831 L712.993036,23.3253012 Z"
+          id="ic_cancel"
+          fill="#8d8d8d"
+        />
       </SvgIcon>
     );
 
     return (
       <div className="note-row">
-          <a href="javascript:void(0);"
-             className="note-link"
-             onFocus= {that.onFocus.bind(that)}
-             tabIndex="0"
-             onClick={this.handleNoteClick.bind(that, this.props.pageId)}
-             onKeyUp={this.arrowKeyPress.bind(that)}>
-              {this.renderIcon(commentExists)}
-              <div className="note-content">
-                  <div>
-                      <p className="dotdotdot">
-                          <strong>{this.props.text}</strong>
-                      </p>
-                  </div>
-                  <div className={commentExists ? '' : 'hide'}>
-                      <p className="user-note">
-                          {this.props.comment}
-                      </p>
-                  </div>
-                  <span className="note-date">
-                    <time value={this.props.time}>{formatDate(new Date(Number(this.props.time)), {
-                      year : 'numeric',
-                      month: 'numeric',
-                      day  : 'numeric'
-                    })}</time>
-                  {' '}
-                    <time value={this.props.time}>{formatTime(new Date(Number(this.props.time)), {
-                      timeZone:that.props.locale,
-                      hour12: true,
-                      hour : 'numeric',
-                      minute: 'numeric'
-                    })}</time>
-                  </span>
-              </div>
-          </a>
-          <a href="javascript:void(0);"
-             className="remove"
-             tabIndex="0"
-             onBlur={that.onBlur.bind(that)}
-             onClick={this.handleModalOpen}
-             aria-label={formatMessage(messages.removeNoteText)}>
-          </a>
-          <Dialog
-            title={formatMessage(messages.confirmDelete)}
-            actions={actions}
-            modal={false}
-            open={this.state.modalOpen}
-            onRequestClose={this.handleModalClose}
-            contentStyle={DialogStyle.dialogContainerstyl}>
-              <CancelIcon tabIndex="0" onClick={this.handleModalClose} viewBox="703 14 18 18.7" style={DialogStyle.cancelIcon} 
-              aria-label={formatMessage(messages.close)} className="handleCloseIcon" onKeyDown={this.cancelIconKeySelect}/>
-              {formatMessage(messages.actionCannotBeUnDone)}
-          </Dialog>
+        <a
+          className="note-link"
+          onFocus={Note.onFocus}
+          tabIndex="0"
+          role="link"
+          onClick={e => this.handleNoteClick(this.props.pageId, e)}
+          onKeyUp={e => this.arrowKeyPress(e)}
+        >
+          {this.renderIcon(commentExists)}
+          <div className="note-content">
+            <div>
+              <p className="dotdotdot">
+                <strong>{this.props.text}</strong>
+              </p>
+            </div>
+            <div className={commentExists ? '' : 'hide'}>
+              <p className="user-note">
+                {this.props.comment}
+              </p>
+            </div>
+            <span className="note-date">
+              <time value={this.props.time}>{formatDate(new Date(Number(this.props.time)), {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric'
+              })}</time>
+              {' '}
+              <time value={this.props.time}>{formatTime(new Date(Number(this.props.time)), {
+                timeZone: that.props.locale,
+                hour12: true,
+                hour: 'numeric',
+                minute: 'numeric'
+              })}</time>
+            </span>
+          </div>
+        </a>
+        <a
+          className="remove"
+          tabIndex="0"
+          role="link"
+          onBlur={e => that.onBlur(e)}
+          onClick={this.handleModalOpen}
+          aria-label={formatMessage(messages.removeNoteText)}
+        >{''}</a>
+        <Dialog
+          title={formatMessage(messages.confirmDelete)}
+          actions={actions}
+          modal={false}
+          open={this.state.modalOpen}
+          onRequestClose={this.handleModalClose}
+          contentStyle={DialogStyle.dialogContainerstyl}
+        >
+          <CancelIcon
+            tabIndex="0"
+            onClick={this.handleModalClose}
+            viewBox="703 14 18 18.7"
+            style={DialogStyle.cancelIcon}
+            aria-label={formatMessage(messages.close)}
+            className="handleCloseIcon"
+            onKeyDown={this.cancelIconKeySelect}
+          />
+          {formatMessage(messages.actionCannotBeUnDone)}
+        </Dialog>
       </div>
-    )
+    );
   }
 }
 Note.propTypes = {
-  drawerCallbacks: React.PropTypes.object
+  drawerCallbacks: PropTypes.object.isRequired,
+  color: PropTypes.string.isRequired,
+  noteClick: PropTypes.func.isRequired,
+  removeNote: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  pageId: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
+  time: PropTypes.number.isRequired
 };
